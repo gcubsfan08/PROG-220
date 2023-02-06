3f3faefd9aab3c69d8707b61c4439932bcc6d64f
Gabe <93947261+gcubsfan08@users.noreply.github.com>
2023-02-06 00:26:28 -0600

    Fibonacci Solution Homework

--- homework/algo/logic.cpp
+++ homework/algo/logic.cpp
@@ -1,6 +1,13 @@
 #include "include/logic.h"
 
-long fibbonacci(const int n) { return 0; }
+long fibbonacci(const int n)
+{ 
+    if (n < 1) return 0;
+    if (n == 1) return n;
+
+    long ret = (fibbonacci(n - 1) + fibbonacci(n - 2));
+    return ret;
+}
 
 int is_prime(const int n) { return 0; }
 
