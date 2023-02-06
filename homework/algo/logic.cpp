93341d83565b6f68b9b7d248fc8e0f1544e8d3e3
Gabe <93947261+gcubsfan08@users.noreply.github.com>
2023-02-06 15:26:57 -0600

    Newer Homework Solution Answer

--- homework/algo/logic.cpp
+++ homework/algo/logic.cpp
@@ -1,12 +1,18 @@
 #include "include/logic.h"
 
-long fibbonacci(const int n)
-{ 
-    if (n < 1) return 0;
-    if (n == 1) return n;
-
-    long ret = (fibbonacci(n - 1) + fibbonacci(n - 2));
-    return ret;
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+ 
+int main()
+{
+    int n = 9;
+    printf("%d", fib(n));
+    getchar();
+    return 0;
 }
 
 int is_prime(const int n) { return 0; }
