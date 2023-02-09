eb1cf2866e948226e8ea23620bdf2d0c9c8e9dc0
Gabe <93947261+gcubsfan08@users.noreply.github.com>
2023-02-09 16:05:58 -0600

    Update logic.cpp

--- homework/algo/logic.cpp
+++ homework/algo/logic.cpp
@@ -1,19 +1,6 @@
 #include "include/logic.h"
 
-int fib(int n)
-{
-    if (n <= 1)
-        return n;
-    return fib(n - 1) + fib(n - 2);
-}
- 
-int main()
-{
-    int n = 9;
-    printf("%d", fib(n));
-    getchar();
-    return 0;
-}
+long fibbonacci(const int n) { return 0; }
 
 int is_prime(const int n) { return 0; }
 
